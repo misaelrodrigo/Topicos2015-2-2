c60b36367087c62740450d020e1f2c8f6633612c
Misael Rodrigo <misaelrodrigo@hotmail.com>
2015-09-28 19:59:40 -0300

    commit inicial

---
+++ android/src/br/edu/integrado/topicos2015_2/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.edu.integrado.topicos2015_2.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.edu.integrado.topicos2015_2.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/br/edu/integrado/topicos2015_2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.integrado.topicos2015_2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.integrado.topicos2015_2.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
